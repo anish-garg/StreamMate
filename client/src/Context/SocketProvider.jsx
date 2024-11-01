84e9fc43eb5f90ae3e1825d1c13968494845e0f0
anish-garg <anishgarg5914@gmail.com>
2024-11-01 20:47:10 +0530

    deploying

--- client/src/Context/SocketProvider.jsx
+++ client/src/Context/SocketProvider.jsx
@@ -11,9 +11,11 @@ export const useSocket = () => {
 }
 
 export const SocketProvider = (props) => {
+    var str1 = "localHost:3000";
+    var str2 = "https://stream-mate-omega.vercel.app/?vercelToolbarCode=B6s2chdL3k_0X8Q";
     // useMemo is a React hook that memoizes (caches) the result of a function, storing it and returning the stored value if the function’s dependencies haven’t changed. This prevents unnecessary re-calculations that could impact performance, especially for expensive operations.
     // For connecting backend to frontend
-    const socket = useMemo(() => io("localHost:3000"), []);
+    const socket = useMemo(() => io(str1 || str2), [str1, str2]);
     return (<SocketContext.Provider value={socket}>
         {props.children}
     </SocketContext.Provider>)
